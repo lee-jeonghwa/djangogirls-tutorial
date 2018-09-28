08e9185d2729b7ebbf48f3b0ccb3286cdd5a7ab0
lee_jeonghwa <lee.jeonghwa02@gmail.com>
2018-09-28 14:50:42 +0900

    Bootstrap 정적파일 추가 및 post_list.html 에 CSS파일 링크

--- app/config/settings.py
+++ app/config/settings.py
@@ -15,7 +15,13 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
+# wkdrhrk /static/<경로>/ 의 URL에서
+# '경로'에 해당하는 부분을 검색할 폴더 폭록에 대한 설정
+STATICFILES_DIRS = [
+    STATIC_DIR
+]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -57,7 +63,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        # Django가 template를 load할 경로 목록
+        # Django가 template를load할 경로 목록
         'DIRS': [
             # app/templates/ 폴더를 가리킴
             TEMPLATES_DIR
@@ -126,3 +132,5 @@ USE_TZ = True # False면 한국시간으로 저장 비추!!!
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+#개발 모드(DEBUG = True)에서 /static으로 시작하는 게 있으면
+# STATICFILES_DIRS리스트 안의 것이 있는지 보고 가져온다
